0d121961d2862840050a624f36b3d0c3f499e043
alexstoliar <ostol@softserveinc.com>
2026-09-04 12:31:35 +0300

    switching to corporate VPC

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lb" "main" {
   name               = "${var.project_name}-${var.environment}-alb"
-  internal           = true
+  internal           = false
   load_balancer_type = "application"
   security_groups    = [var.alb_security_group_id]
   subnets            = var.public_subnet_ids
